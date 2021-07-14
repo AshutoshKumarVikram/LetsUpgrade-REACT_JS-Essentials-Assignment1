3577422128fd49083634b827ea6b160c43d34821
Ashutosh Kumar Vikram <mr.ashutoshkumarvikram@gmail.com>
2021-07-14 07:50:12 +0530

    Assignment 1 Submission

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+const Fruits=()=> {
+    return (
+        <div>
+            <h1> Hello World </h1>
+            <h2>Which Fruit You Like Most?</h2>
+            <Button buttonText="APPLE"/>
+            <Button buttonText="GUAVA"/>
+            <Button buttonText="ORANGE"/>
+            <Button buttonText="BANANA"/>
+            <Button buttonText="GRAPES"/>
+        </div> 
+    )
+}
+
+export default Fruits
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Fruits from "./App"
+ReactDOM.render( <Fruits/>, document.getElementById("root"))
